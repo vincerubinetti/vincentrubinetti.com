34ec71ff1b480abdb99ba9661d94378e9b97f58f
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-11-24 14:22:21 -0500

    re-scaffold

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
+// @ts-check
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import vue from "@astrojs/vue";
-import svgLoader from "vite-svg-loader";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue({ appEntrypoint: "/src/pages/_app" })],
   vite: {
-    plugins: [svgLoader()],
+    plugins: [tailwindcss()],
   },
 });
