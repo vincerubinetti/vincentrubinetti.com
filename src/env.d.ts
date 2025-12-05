3976036c0943d787a575c1448b7444d0874fea66
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-12-05 16:07:23 -0500

    update analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,7 +6,5 @@ declare module "vite-plugin-transform";
 // eslint-disable-next-line
 declare interface Window {
   // eslint-disable-next-line
-  dataLayer: any;
-  // eslint-disable-next-line
   grecaptcha: any;
 }
