e96d18a87940e1ddc790964d86eca39b736d96b4
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-01-20 16:03:44 -0500

    improve easter egg slightly

--- src/software/components/BSOD.vue
+++ src/software/components/BSOD.vue
@@ -1,34 +1,46 @@
 <script setup lang="ts">
-import { ref, watchEffect } from "vue";
-import { useEventListener, useIntervalFn } from "@vueuse/core";
+import { ref } from "vue";
+import { useEventListener } from "@vueuse/core";
 import { range } from "lodash-es";
+import { sleep } from "@/util/misc";
 
 const copies = ref(0);
 
-const { pause, resume } = useIntervalFn(
-  () => {
-    copies.value++;
-    if (copies.value > 10) {
-      pause();
-      copies.value = -1;
-    }
-  },
-  100,
-  { immediate: false },
-);
+const max = 5;
+
+const crop = { x: 0, y: 0, w: 0, h: 0 };
+
+const run = async () => {
+  if (copies.value === 0) {
+    crop.x = window.scrollX;
+    crop.y = window.scrollY;
+    crop.w = window.innerWidth;
+    crop.h = window.innerHeight;
+    document.body.style.filter = "url(#bsod)";
+  }
+  copies.value++;
+  if (copies.value >= max) {
+    document.body.style.filter = "";
+    return;
+  }
+  await sleep(100);
+  run();
+};
 
 useEventListener("hashchange", () => {
-  if (window.location.hash === "#bsod") resume();
-});
-
-watchEffect(() => {
-  document.documentElement.style.filter = copies.value > 0 ? "url(#bsod)" : "";
+  if (window.location.hash === "#bsod") run();
 });
 </script>
 
 <template>
-  <svg v-if="copies > 0" xmlns="http://www.w3.org/2000/svg">
-    <filter id="bsod">
+  <svg v-if="copies > 0" xmlns="http://www.w3.org/2000/svg" class="hidden">
+    <filter
+      id="bsod"
+      filterUnits="userSpaceOnUse"
+      :x="crop.x"
+      :y="crop.y"
+      :width="crop.w"
+    >
       <template v-for="n in range(copies)" :key="n">
         <feOffset
           :in="n === 0 ? 'SourceGraphic' : undefined"
@@ -40,7 +52,7 @@ watchEffect(() => {
     </filter>
   </svg>
   <div
-    v-if="copies === -1"
+    v-if="copies >= max"
     class="fixed inset-0 z-100 grid place-items-center bg-blue-500 p-8 font-sans text-white"
   >
     <div class="flex flex-col gap-8">
