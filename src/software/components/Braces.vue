656780d0b028fe714c68f1abf789f4485367ec3b
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-09-21 15:36:45 -0400

    remove anchor positioning

--- src/software/components/Braces.vue
+++ src/software/components/Braces.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import { useId, useTemplateRef } from "vue";
+import { useTemplateRef } from "vue";
 import { useElementVisibility } from "@vueuse/core";
 import Brace from "./brace.svg?component";
-
-const id = useId();
 
 const container = useTemplateRef("container");
 
@@ -11,28 +9,27 @@ const visible = useElementVisibility(container);
 </script>
 
 <template>
-  <Brace
-    ref="left"
-    class="text-dark absolute top-[anchor(center)] -translate-x-16 -translate-y-1/2 scale-500 transition-all duration-1000 max-md:-translate-x-8"
-    :class="visible ? 'left-[anchor(left)] opacity-25' : 'left-1/2'"
-    :style="{ positionAnchor: `--${id}` }"
-  />
-  <div
-    ref="container"
-    class="flex flex-wrap items-center justify-center gap-8 self-center transition-all duration-1000"
-    :class="
-      visible
-        ? '[clip-path:inset(-10%_-10%_-10%_-10%)]'
-        : '[clip-path:inset(-10%_50%_-10%_50%)]'
-    "
-    :style="{ anchorName: `--${id}` }"
-  >
-    <slot />
+  <div class="relative max-w-max self-center">
+    <Brace
+      ref="left"
+      class="text-dark absolute top-1/2 -translate-x-16 -translate-y-1/2 scale-500 transition-all duration-1000 max-sm:-translate-x-8"
+      :class="visible ? 'left-0 opacity-25' : 'left-1/2'"
+    />
+    <div
+      ref="container"
+      class="flex flex-wrap items-center justify-center gap-8 self-center transition-all duration-1000"
+      :class="
+        visible
+          ? '[clip-path:inset(-10%_-10%_-10%_-10%)]'
+          : '[clip-path:inset(-10%_50%_-10%_50%)]'
+      "
+    >
+      <slot />
+    </div>
+    <Brace
+      ref="right"
+      class="text-dark absolute top-1/2 translate-x-16 -translate-y-1/2 scale-500 -scale-x-500 transition-all duration-1000 "
+      :class="visible ? 'right-0 opacity-25' : 'right-1/2'"
+    />
   </div>
-  <Brace
-    ref="right"
-    class="text-dark absolute top-[anchor(center)] translate-x-16 -translate-y-1/2 scale-500 -scale-x-500 transition-all duration-1000 max-md:translate-x-8"
-    :class="visible ? 'right-[anchor(right)] opacity-25' : 'right-1/2'"
-    :style="{ positionAnchor: `--${id}` }"
-  />
 </template>
